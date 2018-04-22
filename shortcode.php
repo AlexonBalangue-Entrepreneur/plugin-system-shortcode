a81499e95af82bf02d479dc9339f6609c80ec3cd
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-04-22 20:25:58 +0200

    prepare joomla 4 alpha

--- shortcode.php
+++ shortcode.php
@@ -2,9 +2,9 @@
 /**
  * @package	Plugin for Joomla!
  * @subpackage  plg_shortcode
- * @version	4.1.1
+ * @version	5.0 alpha
  * @author	AlexonBalangue.me
- * @copyright	(C) 2012-2015 Alexon Balangue. All rights reserved.
+ * @copyright	(C) 2012-2018 Alexon Balangue. All rights reserved.
  * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
@@ -12,13 +12,22 @@
 defined ('_JEXEC') or die;
 if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);# Add this code For Joomla 3.3.4+
 
-jimport('joomla.plugin.plugin');
-jimport( 'joomla.event.plugin' );
+#use Joomla\CMS\Factory;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Plugin\CMSPlugin;
+#use Joomla\CMS\Language\Text;
 
-class PlgSystemShortcode extends JPlugin
+class PlgSystemShortcode extends CMSPlugin
 {
-	protected $autoloadLanguage = true;
-	
+	#protected $autoloadLanguage = true;
+	/**
+	 * Application object.
+	 *
+	 * @var    JApplicationCms
+	 * @since  3.5
+	 */
+	protected $app;	
 	//public function plgSystemShortcode(&$subject,$config)
 	public function __construct(&$subject,$config)
 	{
@@ -39,11 +48,13 @@ class PlgSystemShortcode extends JPlugin
 
     public function onAfterRender()
     {
-		$app = JFactory::getApplication();
-		$docs = JFactory::getDocument();
-		if( $app->isAdmin() ) {
-			$data = JResponse::getBody();
-			JResponse::setBody($data);			
+		#$app = JFactory::getApplication();
+		$docs = $this->app->getDocument();
+		if( $this->app->isAdmin() ) {
+			#$data = JResponse::getBody();
+			$data = $this->app->getBody();
+			#JResponse::setBody($data);			
+			$this->app->setBody($data);			
 					
 		} else {	
 			$data = JResponse::getBody(); 
@@ -54,7 +65,8 @@ class PlgSystemShortcode extends JPlugin
 			$data = do_bbcodes($data); 
 			$data = str_replace('</html>', $new_html_data . "\n</html>", $data);
 
-			JResponse::setBody($data);
+			#JResponse::setBody($data);
+			$this->app->setBody($data);
 		}
 		$docs->addStyleDeclaration('.grade{text-align:center;margin:15px auto;width:72px;height:72px;font-size:50px;line-height:72px;font-weight:400;color:#fff}.grade-a{background-color:#00A500}.grade-b{background-color:#68D035}.grade-c{background-color:#F8CF00}.grade-d{background-color:#FFA901}.grade-e{background-color:#FF7701}.grade-f,.grade-m,.grade-t,.grade-unknown{background-color:#FF4D41}');
 
