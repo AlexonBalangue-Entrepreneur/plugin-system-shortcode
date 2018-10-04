d0c65544965f6fb6e3114fc24f76bac22b10ac12
Alexon Balangue <alexonbalangue@users.noreply.github.com>
2018-10-04 19:10:53 +0200

    update prepared

--- wp/shortcode.php
+++ wp/shortcode.php
@@ -2,7 +2,7 @@
 /**
  * @package	Plugin for Joomla!
  * @subpackage  plg_shortcode
- * @version	4.2.1
+ * @version	4.3
  * @author	AlexonBalangue.me
  * @copyright	(C) 2012-2016 Alexon Balangue. All rights reserved.
  * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
